5870812d49cc7e8ffd9b4f0330beef20bf42dd2c
eugene <eugene.makarov@yongletechnologies.com>
2018-08-05 18:38:19 -0700

    fetching but failing but fetching

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,17 @@ import Events from './events/Events'
 import Header from './header/Header'
 import { Provider } from './Context'
 import '../styles/main.scss'
+import { URL, API_KEY } from '../../env-config'
 
 class App extends Component {
+
+    componentDidMount() {
+        fetch(`${URL}/find/upcoming_events?key=${API_KEY}`, {
+                mode: 'no-cors'
+            })
+            .then(res => console.log(res))
+    }
+
     render() {
         return (
             <Provider>
